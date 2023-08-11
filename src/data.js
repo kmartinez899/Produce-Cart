6763638de18f1a3390a549d83cbbf7c85ff63d86
kmartinez899 <108234382+kmartinez899@users.noreply.github.com>
2023-08-11 10:17:20 -0500

    finished cart.html

---
+++ src/data.js
@@ -0,0 +1,19 @@
+let shopItemsData = [
+    {
+    id: "strawberries1lb",
+    name: "Strawberries 1lb",
+    img: "https://www.kroger.com/product/images/medium/front/0003338320027"
+}, {
+    id: "strawberries32oz",
+    name: "Strawberries 32oz",
+    img: "https://www.kroger.com/product/images/medium/front/0003338320030"
+}, {
+    id: "blueberries18oz",
+    name: "Blueberries 18oz",
+    img: "https://www.kroger.com/product/images/medium/front/0003338322241"
+}, {
+    id: "blueberries1pt",
+    name: "Blueberries 1pt",
+    img: "https://www.kroger.com/product/images/medium/front/0003338322101"
+}
+];
--- src/main.js
+++ src/main.js
@@ -1,25 +1,5 @@
+// list of all items
 let shop = document.getElementById('shop');
-
-let shopItemsData = [
-    {
-    id: "strawberries1lb",
-    name: "Strawberries 1lb",
-    img: "https://www.kroger.com/product/images/medium/front/0003338320027"
-}, {
-    id: "strawberries32oz",
-    name: "Strawberries 32oz",
-    img: "https://www.kroger.com/product/images/medium/front/0003338320030"
-}, {
-    id: "blueberries18oz",
-    name: "Blueberries 18oz",
-    img: "https://www.kroger.com/product/images/medium/front/0003338322241"
-}, {
-    id: "blueberries1pt",
-    name: "Blueberries 1pt",
-    img: "https://www.kroger.com/product/images/medium/front/0003338322101"
-}
-];
-
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let generateShop =()=>{
